234d6d58011aa0c762d50b6677301415453f9640
Bri <bhilliker@gmail.com>
2021-06-23 15:51:02 -0500

    convert list items into dragable items

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,34 +51,82 @@ $(".list-group").on("click", "p", function() {
   $(".list-group").on("blur", "textarea", function(){
     // get the textarea's current value/text
     var text = $(this)
-      .val()
-      .trim();
-
+    .val()
+    .trim();
+    
     // get the parent ul's id attribute
     var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
-      
-
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+    
+    
     // get the task's position in the list of other li elements
     var index = $(this)
-      .closest(".list-group-item")
-      .index();
-
+    .closest(".list-group-item")
+    .index();
+    
     tasks[status][index].text = text;
     saveTasks();
-
+    
     // recreate p element
     var taskP = $("<p>")
     .addClass("m-1")
     .text(text);
-
+    
     // replace textarea with p element
     $(this).replaceWith(taskP);
   });
 });
 
+// turn columns into sortables
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+  },
+  deactivate: function(event) {
+  },
+  over: function(event) {
+  },
+  out: function(event) {
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+      
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+      
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      
+      console.log(tempArr);
+      
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
 
 
 // modal was triggered
